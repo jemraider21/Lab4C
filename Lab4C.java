60d80be7144ae32b4086672742c3330d6ef41251
Jared (Desktop) <jemraider21@gmail.com>
2019-04-10 05:51:29 -0400

    Program works with sending a string array into method. Need to adjust for list of string

--- Lab4C.java
+++ Lab4C.java
@@ -7,52 +7,54 @@
  * matches one in the list, the program is to alert the user that the name is 
  * taken. If the name 
  */
-import java.util.List;
+//import java.util.List;
 import java.util.Arrays;
+//import java.util.Collections;
 import java.util.Scanner;
 
 public class Lab4C{
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);  
         String[] bees = {"Angel", "Thurmon", "Nancy", "Bubba", "Thelonius",
-          "Caluci", "Cassius", "Jojo", "Zeke", "Yancy", "Diane", "Xavier eXeter",
-          "Elaine Elosky", "Cardi Bee"};
-        String temp;
-        for (int i = 1; i < bees.length; i++){
-            for(int j = i; j > 0; j--){
-
-              if(bees[j].compareTo(bees[j - 1]) == -1){
-                    temp = bees[j];
-                    bees[j] = bees[j - 1];
-                    bees[j-1] = temp;
-                }
-            }
-        }
-        //List <String> customerList = Arrays.asList(bees);
-        Scanner input = new Scanner(System.in);
+            "Caluci", "Cassius", "Jojo", "Zeke", "Yancy", "Diane", "Xavier eXeter",
+            "Elaine Elosky", "Cardi Bee"};
+        int looper = 1;
         
-
-        System.out.print("Enter a bee's name: ");
-        String name  = input.nextLine();
-        int low = 0;
-        int high = bees.length - 1;
-        int result = beeNameSearch(bees, name, low, high);
-
-        System.out.println(result);
-        if (result == -1){
-          System.out.println("You got a bee!");
-        } else {
-          System.out.println("Bee more creative! This name is taken...");
+        // Sort the array of bee names 
+        String[] sortedBees = new String[bees.length];
+        for (int i = 0; i < bees.length; i++){
+          sortedBees[i] = bees[i];
         }
 
+        /*List <String> beeNames = Arrays.asList(bees);
+        Collections.sort(beeNames);*/
 
+        do{
+            System.out.print("Enter a bee's name: ");
+            String name  = input.nextLine();
+    
+            if (name.equals("0")){ // Break the loop
+                looper = 0;
+            } else {
+                int low = 0;
+                int high = bees.length - 1;
+                int result = beeNameSearch(sortedBees, name, low, high);
+    
+                if (result == -1){
+                  System.out.println("You got a bee!");
+                } else{
+                  System.out.println("Bee more creative! This name is taken...");
+                }
+                looper = 1;
+            }
+        } while(looper != 0);
+    
         input.close();
         System.out.println("Goodbye!");
     } // End main method
 
     public static int beeNameSearch(String[] list, String name, int low, int high){
         int middle = (low + high +1) / 2; 
-        int checker = name.compareTo(list[middle]); 
-        System.out.println(checker);
         /*
           If checker == -1(name < list[middle]), high will equal middle minus 1
           If checker == 0 (name == list[middle]), location will equal middle
@@ -61,16 +63,13 @@ public class Lab4C{
         if (high < low){
             return -1;
         }
-        if (checker == 0){
-            return middle;
-        } 
-        if (checker == -1){
-            return beeNameSearch(list, name, low, middle - 1);
-        } 
-        if (checker == 1) {
-            return beeNameSearch(list, name, middle + 1, high);
-        }
 
-        return -1;
+        if (name.equals(list[middle])){ // If the two strings are equal to each other
+            return middle;
+        } else if (name.compareTo(list[middle]) < 0){ // If name is less than the midpoint of the array
+            return beeNameSearch(list, name, low, middle - 1);
+        } else{ // If name is greater than the midpoint of the array
+            return beeNameSearch(list, name, middle + 1, high); 
+        }
     } // End beeNameChecker method
 } // End Lab4C class
